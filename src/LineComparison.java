e88b2cbffb989280221e811a9a56312f3eaa6920
swapnil yadav <swapnilmaya19compeng@student.mes.ac.in>
2024-05-16 13:02:31 +0530

    UC2 Checking two lines are equal or not

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,18 +1,23 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class LineComparison {
-    public void isEqual(double x1,double x2,double y1,double y2){
+    public Double isEqual(double x1,double x2,double y1,double y2){
 
 
         double length_line = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
-        System.out.println("Length of line is :"+length_line);
+        return length_line;
     }
 
     public static void main(String[] args) {
 
         LineComparison uc1 = new LineComparison();
-        uc1.isEqual(1, 5, 4, 9);
-
-
+        double length_line1 = uc1.isEqual(1, 5, 4, 9);
+        double length_line2 = uc1.isEqual(2,6,4,8);
+        if(length_line1 == length_line2){
+            System.out.println("Lines are Equal");
+        }
+        else{
+            System.out.println("Lines are Not Equal");
+        }
     }
 }
