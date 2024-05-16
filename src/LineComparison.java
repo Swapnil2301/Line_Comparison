61a2c6a74d7408687e77db2969a20bd35e6d38e9
swapnil yadav <swapnilmaya19compeng@student.mes.ac.in>
2024-05-16 12:34:10 +0530

    UC1 Added Function to Calculating Length of line

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,9 +1,17 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class LineComparison {
+    public void isEqual(double x1,double x2,double y1,double y2){
+
+
+        double length_line = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+        System.out.println("Length of line is :"+length_line);
+    }
+
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Line Comparison Comupution");
+        LineComparison uc1 = new LineComparison();
+        uc1.isEqual(1, 5, 4, 9);
 
 
     }
