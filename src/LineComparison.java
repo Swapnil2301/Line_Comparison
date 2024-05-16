9235da27b026e33969f06b39b123b21a95d41d7d
swapnil yadav <swapnilmaya19compeng@student.mes.ac.in>
2024-05-16 12:27:05 +0530

    File added

---
+++ src/LineComparison.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class LineComparison {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Line Comparison Comupution");
+
+
+    }
+}
